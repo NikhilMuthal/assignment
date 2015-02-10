16d4ca50d2e10fc8cf22e6a0df30814035d81794
nikhilmuthal <nikhil.muthal07@gmail.com>
2015-02-11 02:05:54 +0530

    Added Twitter Implimentation

--- Gemfile
+++ Gemfile
@@ -12,8 +12,8 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
-
+gem 'therubyracer', platforms: :ruby
+gem "tzinfo-data"
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
@@ -29,9 +29,12 @@ gem 'will_paginate', '~> 3.0.6'
 gem 'will_paginate-bootstrap'
 gem 'twitter'
 gem "httparty"
+gem 'coffee-script-source', '1.8.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'pry-rails'
+gem 'wicked_pdf'
+gem 'wkhtmltopdf-binary'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
@@ -40,7 +43,7 @@ gem "httparty"
 
 group :development, :test do
   # Call 'debugger' anywhere in the code to stop execution and get a debugger console
-  gem 'debugger'
+  #gem 'debugger'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -12,6 +12,7 @@ class SearchResultsController < ApplicationController
      search_query = params[:search]
      get_github_user(search_query)
      ruby_gems_owners(search_query)
+     twitter_news(search_query)
   end
  
   def get_github_user(search)
@@ -25,22 +26,21 @@ class SearchResultsController < ApplicationController
     
     @repos = @repos.paginate(:page => params[:github_page], :per_page => 10)
   end
-  
-  
-  def twitter_news
+    
+  def ruby_gems_owners(search)
+    base_uri = "https://rubygems.org/api/v1/owners/#{search}/gems.json"
+    @response = HTTParty.get("#{base_uri}").parsed_response
+    @response = @response.paginate(:page => params[:rubyGems_page], :per_page => 10)
+  end
+
+  def twitter_news(search)
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = "UblzqTDa7NvGjN0MZZs1wDSgY"
       config.consumer_secret     = "E0e33TiG3TcZ6BMeWEFTleZcz1hczAxWWOLwmuhAZ3j0sIEeEV"
       config.access_token        = "197863336-d7GOniO5X0X82k8jFezRGRLYLvJ5g3ePfGiWRNKO"
       config.access_token_secret = "DVz7LzygKsBm1fOcWZURaW8LO6LLa6SkSYnPEDG2jir2m"
     end
-    @twitter_user = client.user(params[:search])
+    @twitter_user = client.user_timeline(search, :count => 10)
+    @twitter_user = @twitter_user.paginate(:page => params[:twitter_page], :per_page => 10)
   end
-  
-  def ruby_gems_owners(search)
-    base_uri = "https://rubygems.org/api/v1/owners/#{search}/gems.json"
-    @response = HTTParty.get("#{base_uri}").parsed_response
-    @response = @response.paginate(:page => params[:rubyGems_page], :per_page => 10)
-  end
-  
 end
